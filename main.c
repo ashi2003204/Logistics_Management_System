b25a9de527cebbd19855811c2b5c5ad16e8fa9cb
Methmi Ashinsana <as20240663204@sci.sjp.ac.lk>
2025-10-30 22:44:20 +0530

    Add file handling to save and load cities,distance,and deliveries

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ const int VEHICLE_RATE[3] = {30, 40, 80};
 const int VEHICLE_SPEED[3] = {60, 50, 45};
 const int VEHICLE_EFFICIENCY[3] = {12, 6, 4};
 
-#define FUEL_PRICE 310.0
+#define FUEL_PRICE 310.0r
 
 
 int distance[MAX_CITIES][MAX_CITIES];
@@ -39,6 +39,75 @@ typedef struct {
 
 Delivery deliveries[MAX_DELIVERIES];
 int delivery_count = 0;
+
+
+void save_data() {
+    FILE *fc = fopen("cities.txt", "w");
+    if (fc) {
+        fprintf(fc, "%d\n", city_count);
+        for (int i = 0; i < city_count; i++)
+            fprintf(fc, "%s\n", cities[i]);
+        fclose(fc);
+    }
+
+    FILE *fd = fopen("distances.txt", "w");
+    if (fd) {
+        fprintf(fd, "%d\n", city_count);
+        for (int i = 0; i < city_count; i++) {
+            for (int j = 0; j < city_count; j++)
+                fprintf(fd, "%d ", distance[i][j]);
+            fprintf(fd, "\n");
+        }
+        fclose(fd);
+    }
+
+    FILE *fl = fopen("deliveries.txt", "w");
+    if (fl) {
+        fprintf(fl, "%d\n", delivery_count);
+        for (int i = 0; i < delivery_count; i++) {
+            Delivery d = deliveries[i];
+            fprintf(fl, "%d %d %d %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f\n",
+                    d.source, d.destination, d.vehicleType, d.weight, d.baseCost,
+                    d.time, d.fuelUsed, d.fuelCost, d.totalOperationalCost,
+                    d.profit, d.customerCharge, d.distance);
+        }
+        fclose(fl);
+    }
+}
+
+void load_data() {
+    FILE *fc = fopen("cities.txt", "r");
+    if (fc) {
+        fscanf(fc, "%d", &city_count);
+        for (int i = 0; i < city_count; i++)
+            fscanf(fc, "%s", cities[i]);
+        fclose(fc);
+    }
+
+    FILE *fd = fopen("distances.txt", "r");
+    if (fd) {
+        int count;
+        fscanf(fd, "%d", &count);
+        for (int i = 0; i < count; i++)
+            for (int j = 0; j < count; j++)
+                fscanf(fd, "%d", &distance[i][j]);
+        fclose(fd);
+    }
+
+    FILE *fl = fopen("deliveries.txt", "r");
+    if (fl) {
+        fscanf(fl, "%d", &delivery_count);
+        for (int i = 0; i < delivery_count; i++) {
+            Delivery d;
+            fscanf(fl, "%d %d %d %f %f %f %f %f %f %f %f %f",
+                   &d.source, &d.destination, &d.vehicleType, &d.weight, &d.baseCost,
+                   &d.time, &d.fuelUsed, &d.fuelCost, &d.totalOperationalCost,
+                   &d.profit, &d.customerCharge, &d.distance);
+            deliveries[i] = d;
+        }
+        fclose(fl);
+    }
+}
 
 
 void add_city() {
@@ -84,8 +153,6 @@ void input_distance() {
     scanf("%d", &d);
     distance[i][j] = distance[j][i] = d;
     printf("Distance updated!\n");
-}
-
 
 void display_vehicles() {
     printf("\nAvailable Vehicles:\n");
@@ -177,6 +244,8 @@ void create_delivery() {
     printf("Total Operational Cost: %.2f LKR\nProfit: %.2f LKR\nCustomer Charge: %.2f LKR\nEstimated Time: %.2f hours\n",
            d.totalOperationalCost, d.profit, d.customerCharge, d.time);
     printf("---------------------------------\n");
+
+    save_data();
 }
 
 
@@ -218,23 +287,24 @@ void main_menu() {
         printf("3. View Vehicles\n");
         printf("4. Create Delivery\n");
         printf("5. View Reports\n");
-        printf("6. Exit\n");
+        printf("6. Exit (Save & Quit)\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1: add_city(); break;
-            case 2: input_distance(); break;
+            case 1: add_city(); save_data(); break;
+            case 2: input_distance(); save_data(); break;
             case 3: display_vehicles(); break;
             case 4: create_delivery(); break;
             case 5: show_reports(); break;
-            case 6: exit(0);
+            case 6: save_data(); printf("Data saved. Exiting...\n"); exit(0);
             default: printf("Invalid choice!\n");
         }
     }
 }
 
 int main() {
+    load_data();
     main_menu();
     return 0;
 }
